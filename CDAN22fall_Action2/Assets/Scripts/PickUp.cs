ac1f7f283005b011161a64776774309275fa344d
Rubykscube <115187238+Rubykscube@users.noreply.github.com>
2022-12-03 14:22:10 -0500

    sound pick up

--- CDAN22fall_Action2/Assets/Scripts/PickUp.cs
+++ CDAN22fall_Action2/Assets/Scripts/PickUp.cs
@@ -2,39 +2,34 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
-public class PickUp : MonoBehaviour{
+public class PickUp_UpDown : MonoBehaviour{
 
-      public GameHandler gameHandler;
-	  public GameTracking gameTracking;
-      //public GameObject TextBox;
-      //public playerVFX playerPowerupVFX;
-      public bool isKey1 = false;
-	  public bool isKey2 = false;
+      //initial values, can be made public
+      private Vector3 startPos;
+
+      public float speedS = 2.5f;
+      public float distS = 0.5f;
+
+      //extra parameters for randomizing movement
+      private float randBottom = 0.6f;
+      private float randTop = 1f;
+
+      private bool SinWaveMove = true;
+      private bool randomizeSin = true;
 
       void Start(){
-            gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
-			gameTracking = GameObject.FindWithTag("GameHandler").GetComponent<GameTracking>();
-            //playerPowerupVFX = GameObject.FindWithTag("Player").GetComponent<playerVFX>();
+            startPos = transform.position;
+
+            //randomize distance
+            if (randomizeSin==true){
+                  speedS = (speedS * Random.Range(randBottom, randTop));
+            }
       }
 
-      public void OnTriggerEnter2D (Collider2D other){
-            if (other.gameObject.tag == "Player"){
-                  GetComponent<Collider2D>().enabled = false;
-                  
-				if (isKey1 == true){  
-				  GameTracking.hasFoundKey1=true;
-				  gameTracking.hasFoundKey1_test=true;
-				}
-				if (isKey2 == true){  
-				  GameTracking.hasFoundKey2=true;
-				}
-				  
-				  StartCoroutine(DestroyThis());
-              }
-          }
-
-      IEnumerator DestroyThis(){
-            yield return new WaitForSeconds(0.3f);
-            Destroy(gameObject);
+      void Update (){
+            //use Mathf.Sin function to move up and down
+            if (SinWaveMove == true){
+                  transform.position = startPos + new Vector3(0.0f, (Mathf.Sin(Time.time * speedS) * distS), 0.0f);
+            }
       }
-  }
+}
