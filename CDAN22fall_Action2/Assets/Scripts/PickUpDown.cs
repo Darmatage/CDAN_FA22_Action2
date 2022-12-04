0841c9b57e52b08aac06d91d254aa743813d9025
TwistedDrawer <115186967+TwistedDrawer@users.noreply.github.com>
2022-12-04 10:32:11 -0500

    Fixed PickUpDown Name Error
    Script was name PickUp_UpDown Inside the script instead of PickUpDown and it was confused

--- CDAN22fall_Action2/Assets/Scripts/PickUpDown.cs
+++ CDAN22fall_Action2/Assets/Scripts/PickUpDown.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
-public class PickUp_UpDown : MonoBehaviour{
+public class PickUpDown : MonoBehaviour{
 
       //initial values, can be made public
       private Vector3 startPos;
