6f462aab117688cc2a7c8b9b1f8d945b038fe1d6
livthealien <fairolivia4@gmail.com>
2022-12-15 23:09:09 -0500

    Updated shop script + shop UI

--- CDAN22fall_Action2/Assets/Scripts/DoorExitSimple.cs
+++ CDAN22fall_Action2/Assets/Scripts/DoorExitSimple.cs
@@ -25,11 +25,6 @@ public class DoorExitSimple : MonoBehaviour{
 		if (GameTracking.isHallway){
 			canEnter=true;
 		}
-      if (GameTracking.isHallway){
-        if(GameTracking.finishedAllPuzzles==false){
-          canEnter=false;
-        }
-      }
 
 // room 1
 
