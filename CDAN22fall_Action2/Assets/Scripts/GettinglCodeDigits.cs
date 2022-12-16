1b134db5d3f410173ed05bebf1d2c17adf29f7d0
livthealien <fairolivia4@gmail.com>
2022-12-16 12:26:44 -0500

    GettingCodeDigits code updated

--- CDAN22fall_Action2/Assets/Scripts/GettinglCodeDigits.cs
+++ CDAN22fall_Action2/Assets/Scripts/GettinglCodeDigits.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class GettingCodeDigits : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
 
+  public GameObject codeNumber1;
+  public GameObject codeNumber2;
+  public GameObject codeNumber3;
+  public GameObject codeNumber4;
+
+    // Start is called before the first frame update
+    void Start(){
+        codeNumber1.SetActive(false);
+        codeNumber2.SetActive(false);
+        codeNumber3.SetActive(false);
+        codeNumber4.SetActive(false);
     }
 
     // Update is called once per frame
