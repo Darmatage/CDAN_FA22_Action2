e994e7ce7b0100270e74c6808804c7e23e8a7b58
livthealien <fairolivia4@gmail.com>
2022-12-17 13:03:24 -0500

    Added win screen art + fixed timer

--- CDAN22fall_Action2/Assets/Scripts/GameTimer.cs
+++ CDAN22fall_Action2/Assets/Scripts/GameTimer.cs
@@ -2,9 +2,10 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameTimer : MonoBehaviour {
-	public int timer = 0;
+	private int timer = 240;
 	private float theTimer = 0f;
 	public GameObject timerText;
 	public bool canTimer = true;
@@ -12,24 +13,35 @@ public class GameTimer : MonoBehaviour {
 	public GameObject timerTextObj;
 
 
-	void Start (){	
-		if (canTimer==false){timerTextObj.SetActive(false);} 
-		else {timerTextObj.SetActive(true);}
+	void Start (){
+		if (canTimer==false){timerTextObj.SetActive(false);}
+		else {
+			timerTextObj.SetActive(true);
+			UpdateTimer();
+		}
 	}
 
 	void FixedUpdate(){
 		   if (canTimer){
               theTimer += 0.01f;
               if (theTimer >= 1f){
-                     timer +=1;
+                     timer -=1;
                      theTimer = 0;
                      UpdateTimer();
               }
 		   }
-      }
+			 if (timer <= 0){
+				 LoseGame();
+			 }
+    }
 
       public void UpdateTimer(){
             Text timeTextTemp = timerText.GetComponent<Text>();
             timeTextTemp.text = "" + timer;
       }
+
+		public void LoseGame(){
+				SceneManager.LoadScene("LoseScreen");
+		}
+
 }
