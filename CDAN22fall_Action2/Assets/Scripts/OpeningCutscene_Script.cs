4551427bd0df119e92b101f9f0f15099efd9ad1c
livthealien <fairolivia4@gmail.com>
2022-12-21 03:29:35 -0500

    Finished opening cutscene script + functionality and fixed game handler canvas UI layout

--- CDAN22fall_Action2/Assets/Scripts/OpeningCutscene_Script.cs
+++ CDAN22fall_Action2/Assets/Scripts/OpeningCutscene_Script.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class OpeningCutscene_Script : MonoBehaviour{
 
   public int primeInt = 1;
   public GameObject textBoxDisplay;
   public GameObject nextButton;
+  public GameObject nextSceneButton;
 
-  public string textBoxText;
+  public Text OpeningText;
   private bool allowSpace = true;
   //private bool isEndofScene = false;
 
@@ -16,13 +19,14 @@ public class OpeningCutscene_Script : MonoBehaviour{
     void Start(){
         textBoxDisplay.SetActive(true);
         nextButton.SetActive(true);
+        nextSceneButton.SetActive(false);
 
       if (allowSpace==true){
         if(Input.GetKeyDown("space")){
           talking();
         }
       }
-      }
+    }
 
   public void talking(){
     primeInt = primeInt + 1;
@@ -30,25 +34,49 @@ public class OpeningCutscene_Script : MonoBehaviour{
     if (primeInt==1){
     }
       else if (primeInt==2){
-        //textBoxText = "I've been inside these walls too long now.";
+        //StartCoroutine(TypeText(OpeningText.text, "(I've been inside these walls too long now.)"));
+        OpeningText.text = "(I've been inside these walls too long now.)";
       }
       else if (primeInt==3){
-        //textboxText = "I want to venture out and see the world.";
+        //StartCoroutine(TypeText(OpeningText.text, "(I want to venture out and see the world.)"));
+        OpeningText.text = "(I want to venture out and see the world.)";
       }
       else if (primeInt==4){
-        //textboxText = "But first, I have to find a way outside this house.";
+        //StartCoroutine(TypeText(OpeningText.text, "(But first, I have to find a way outside this house.)"));
+        OpeningText.text = "(But first, I have to find a way outside this house.)";
       }
       else if (primeInt==5){
-        //textboxText = "Alright. ... ";
+        //StartCoroutine(TypeText(OpeningText.text, "(Alright. ... )"));
+        OpeningText.text = "(Alright. ... )";
       }
-      else if (primeInt==5){
-        //textboxText = "Here I go.";
+      else if (primeInt==6){
+        //StartCoroutine(TypeText(OpeningText.text, "(Here I go.)"));
+        OpeningText.text = "(Here I go.)";
+      }
+      else if (primeInt==7){
+        OpeningText.text = "(Here I go.)";
 
+        nextButton.SetActive(false);
+        nextSceneButton.SetActive(true);
         //isEndofScene=true;
-        //LoadScene();
+        allowSpace=false;
+        //isEndofScene=true;
+        LoadScene();
       }
   }
 
-  //public void LoadScene(){
-    //SceneManager.LoadScene(Level1Bedroom_new);
+  public void LoadScene(){
+    SceneManager.LoadScene("Level1Bedroom_new");
   }
+
+  //IEnumerator TypeText(Text target, string fullText){
+             //float delay = 0.01f;
+             //nextButton.SetActive(false);
+             //allowSpace = false;
+             //for (int i = 0; i < fullText.Length; i++){
+                     //string currentText = fullText.Substring(0,i);
+                     //target.text = currentText;
+                     //yield return new WaitForSeconds(delay);
+             //}
+           //}
+}
