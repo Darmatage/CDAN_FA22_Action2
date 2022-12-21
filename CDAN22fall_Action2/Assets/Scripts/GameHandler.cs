d1204e9960c455a08865797bdd3597ca074eb6eb
livthealien <fairolivia4@gmail.com>
2022-12-21 03:32:52 -0500

    Made Play button in main menu go to opening cutscene + added opening cutscene to build settings

--- CDAN22fall_Action2/Assets/Scripts/GameHandler.cs
+++ CDAN22fall_Action2/Assets/Scripts/GameHandler.cs
@@ -100,7 +100,7 @@ public class GameHandler : MonoBehaviour{
       }
 
       public void StartGame() {
-            SceneManager.LoadScene("Level1Bedroom_new");
+            SceneManager.LoadScene("OpeningCutscene");
       }
 
       public void RestartGame() {
